e6049cf32d66592336eca80f1a08bec24518db91
Feijaoozin <gonardo1234@gmail.com>
2025-07-13 00:19:53 -0300

    feat: initial project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,134 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import {
+  Alert,
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import Item from "./src/item";
+
+import { createStore } from "tinybase";
+import { useCreatePersister } from "tinybase/ui-react";
+import * as SQLite from "expo-sqlite";
+import { createExpoSqlitePersister } from "tinybase/persisters/persister-expo-sqlite";
+
+const TABLE_NAME = "products";
+const store = createStore();
+
+type ProductStore = {
+  description: string;
+  done: boolean;
+};
+
+type Product = ProductStore & { id: string };
 
 export default function App() {
+  const [description, setDescription] = useState("");
+  const [products, setPoducts] = useState<Product[]>([]);
+
+  useCreatePersister(
+    store,
+    (store) =>
+      createExpoSqlitePersister(store, SQLite.openDatabaseSync("database.db")),
+    [],
+    // @ts-ignore
+    (persister) => persister.load().then(persister.startAutoSave)
+  );
+
+  function get() {
+    const data = store.getTable(TABLE_NAME);
+    const response = Object.entries(data).map(([id, product]) => ({
+      id,
+      description: String(product.description),
+      done: Boolean(product.done),
+    }));
+    setPoducts(response);
+  }
+
+  function add() {
+    if (description.trim() === "") {
+      return Alert.alert("Atenção", "Informe o produto");
+    }
+
+    const id = Math.random().toString(30).substring(2, 20);
+    store.setRow(TABLE_NAME, id, { description, done: false });
+
+    setDescription("");
+    // get();
+  }
+
+  function remove(id: string) {
+    store.delRow(TABLE_NAME, id);
+  }
+
+  function toggleStatus(id: string) {
+    const product = store.getRow(TABLE_NAME, id);
+    store.setRow(TABLE_NAME, id, { ...product, done: !product.done });
+  }
+
+  useEffect(() => {
+    const listener = store.addTableListener(TABLE_NAME, get);
+    get();
+
+    return () => {
+      store.delListener(listener);
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.form}>
+        <TextInput
+          placeholder="O que você precisa comprar?"
+          onChangeText={setDescription}
+          value={description}
+          style={styles.input}
+        />
+
+        <Button title="Adicionar" onPress={add} />
+      </View>
+
+      <FlatList
+        data={products}
+        renderItem={({ item }) => (
+          <Item
+            data={item}
+            onStatus={() => toggleStatus(item.id)}
+            onRemove={() => remove(item.id)}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.empty}>Nenhum item.</Text>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+  },
+
+  form: {
+    width: "100%",
+    alignItems: "center",
+    marginTop: 100,
+    gap: 16,
+    marginBottom: 50,
+  },
+
+  input: {
+    backgroundColor: "#d3d3d3",
+    borderWidth: 1,
+    width: "90%",
+    fontWeight: "bold",
+  },
+  empty: {
+    color: "#888888",
   },
 });
---
+++ src/item.tsx
@@ -0,0 +1,53 @@
+import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+type ItemData = {
+  done: boolean;
+  description: string;
+};
+
+type Props = {
+  data: ItemData;
+  onStatus: () => void;
+  onRemove: () => void;
+};
+
+export default function Item({ data, onStatus, onRemove }: Props) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.container2}>
+        <TouchableOpacity activeOpacity={0.8} onPress={onStatus}>
+          <MaterialIcons
+            name={data.done ? "check-box" : "check-box-outline-blank"}
+            size={24}
+            color="#828282"
+          />
+        </TouchableOpacity>
+
+        <Text style={styles.description}>{data.description}</Text>
+      </View>
+
+      <TouchableOpacity activeOpacity={0.8} onPress={onRemove}>
+        <MaterialIcons name="delete" size={24} color="#828282" />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    minWidth: "85%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 24,
+  },
+  container2: {
+    flexDirection: "row",
+    gap: 8,
+  },
+  description: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#555555",
+  },
+});
